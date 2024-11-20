cf3a737c291e8092253ebb430eb849795c50417b
AbuRayhan1262000 <zahirrayhan301@gmail.com>
2024-11-20 13:00:34 +0600

    Asynchronous_js

---
+++ Asynchronous JS/Async-function/Async.js
@@ -0,0 +1,20 @@
+
+task1 = function(){
+    console.log("task1 is completed");
+}
+task2 = function(){
+    setTimeout(()=>{
+        console.log("task2 is completed");
+        
+    },0)
+}
+task3 = function(){
+    console.log("task3 is completed");
+}
+task4 = function(){
+    console.log("task4 is completed");
+}
+task1();
+task2();
+task3();
+task4();
